f38ecab158c9399b04224bea18f08ba14adad965
Jose Javier Paez <josejavierpaez@Joses-MacBook-Pro.local>
2020-03-03 00:31:20 -0400

    server routes config

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+//* settings
+app.set("port", process.env.PORT || 4000);
+
+//* middlewares
+app.use(cors());
+app.use(express.json());
+
+//* routes
+app.get("/api/users", (req, res) => res.send("Users Routes"));
+app.get("/api/notes", (req,res) => res.send("Notes routes"));
 
 module.exports = app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const app = require("./app");
 require("./database");
-const PORT = 4000;
 const colors = require("colors");
+const PORT = app.get("port");
+
 
 async function Main() {
   await app.listen(PORT, () => console.log(`server on port ${PORT}`.green));
