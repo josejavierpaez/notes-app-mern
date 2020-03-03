ae170b2de6b82d7a05d2c36acff4d39caca16cc9
Jose Javier Paez <josejavierpaez@Joses-MacBook-Pro.local>
2020-03-03 01:03:14 -0400

    refactoring routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 //* routes
-app.get("/api/users", (req, res) => res.send("Users Routes"));
-app.get("/api/notes", (req,res) => res.send("Notes routes"));
+app.use("/api/users",require("./routes/users"));
+app.use("/api/notes",require("./routes/notes"));
 
 module.exports = app;
