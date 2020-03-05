55248801f2e9652c3a32e5449730f4abbe2dfea6
josejavierpaez <josejavierpaez@hotmail.com>
2020-03-05 17:09:45 -0400

    handle promise

--- backend/src/routes/notes.js
+++ backend/src/routes/notes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+const response = require("../response");
 const {
   getNotes,
   createNote,
@@ -7,16 +8,33 @@ const {
   updateNote,
   getNote
 } = require("../controllers/notesControllers");
-router
-  .route("/")
-  .get(getNotes)
-  .post((req, res) => res.json({ message: "get request" }));
+router.route("/").get((req, res) => {
+  getNotes().then(notes => {
+    response.success(req, res, notes, 200);
+  });
+});
 
 router
   .route("/:id")
-  .get(getNote)
-  .post(createNote)
-  .put(updateNote)
-  .delete(deleteNote);
+  .get((req, res) => {
+    getNote().then(note => {
+      response.success(req, res, note, 200);
+    });
+  })
+  .post((req, res) => {
+    createNote().then(notes => {
+      response.success(req, res, notes, 200);
+    });
+  })
+  .put((req, res) => {
+    updateNote().then(notes => {
+      response.success(req, res, notes, 200);
+    });
+  })
+  .delete((req, res) => {
+    deleteNote().then(notes => {
+      response.success(req, res, notes, 200);
+    });
+  });
 
 module.exports = router;
--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,18 +1,37 @@
 const { Router } = require("express");
 const router = Router();
+const response = require("../response");
 const {
   createUser,
   getUsers,
   deleteUser
 } = require("../controllers/usersControllers");
 
-router
-.route("/")
-.get(getUsers);
+router.route("/").get((req, res) => {
+  getUsers().then(users => {
+    response.success(req, res, users, 201);
+  });
+});
 
 router
   .route("/:id")
-  .post(createUser)
-  .delete(deleteUser);
+  .post((req, res) => {
+    createUser()
+      .then(user => {
+        response.success(req, res, user, 200);
+      })
+      .catch(error => {
+        response.error(req, res, error, 500, "error into controller");
+      });
+  })
+  .delete((req, res) => {
+    deleteUser()
+      .then(user => {
+        response.success(req, res, user, 200);
+      })
+      .catch(error => {
+        response.error(req, res, error, 500, "error into controller");
+      });
+  });
 
 module.exports = router;
