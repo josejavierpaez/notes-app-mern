87f02c125391c1be7db308a3006932c75838e406
josejavierpaez <josejavierpaez@hotmail.com>
2020-03-07 04:26:38 -0400

    fix sintax

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 //* routes
-app.use("/api/users",require("./routes/users"));
-app.use("/api/notes",require("./routes/notes"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/notes", require("./routes/notes"));
 
 module.exports = app;
