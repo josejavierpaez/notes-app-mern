f10c1135ed033631d999050e7614195ee6399649
josejavierpaez <josejavierpaez@hotmail.com>
2020-03-09 17:25:33 -0400

    add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import NotesList from "./components/NotesList";
+import CreateNode from "./components/CreateNote";
+import CreateUser from "./components/CreateUser";
 
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
+    <Router>
+      <Navigation />
+
+      <Route path="/" exact component={NotesList} />
+      <Route path="/edit/:id" component={CreateNode} />
+      <Route path="/create" component={CreateNode} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
   );
 }
 
